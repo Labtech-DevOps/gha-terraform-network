b89036c7806468e6008bd4510e375baa9849c1d3
pcb737 <63125424+pcb737@users.noreply.github.com>
2024-02-21 13:29:50 -0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -16,6 +16,7 @@ monorepo_url="$INPUT_MONOREPOURL"
 scaffold_directory="$INPUT_SCAFFOLDDIRECTORY"
 create_port_entity="$INPUT_CREATEPORTENTITY"
 branch_name="port_$port_run_id"
+git_url="$INPUT_GITHUBURL"
 
 get_access_token() {
   curl -s --location --request POST 'https://api.getport.io/v1/auth/access_token' --header 'Content-Type: application/json' --data-raw "{
@@ -42,15 +43,7 @@ add_link() {
     --data "{
       \"link\": \"$url\"
     }"
-
-  # Verifica o código de retorno do curl
-  if [ $? -eq 0 ]; then
-    echo "Comando executado com sucesso (código de retorno zero)."
-  else
-    echo "Erro ao executar o comando (código de retorno diferente de zero)."
-  fi
 }
-
 
 create_repository() {  
   resp=$(curl -H "Authorization: token $github_token" -H "Accept: application/json" -H "Content-Type: application/json" $git_url/users/$org_name)
@@ -72,6 +65,106 @@ create_repository() {
   else
     echo "Invalid user type"
   fi
+  echo "$resp"
+  echo "$userType"
+}
+
+clone_monorepo() {
+  git clone $monorepo_url monorepo
+  cd monorepo
+  git checkout -b $branch_name
+}
+
+prepare_cookiecutter_extra_context() {
+  echo "$port_user_inputs" | jq -r 'with_entries(select(.key | startswith("cookiecutter_")) | .key |= sub("cookiecutter_"; ""))'
+}
+
+cd_to_scaffold_directory() {
+  if [ -n "$monorepo_url" ] && [ -n "$scaffold_directory" ]; then
+    cd $scaffold_directory
+  fi
+}
+
+apply_cookiecutter_template() {
+  extra_context=$(prepare_cookiecutter_extra_context)
+
+  echo "🍪 Applying cookiecutter template $cookie_cutter_template with extra context $extra_context"
+  # Convert extra context from JSON to arguments
+  args=()
+  for key in $(echo "$extra_context" | jq -r 'keys[]'); do
+      args+=("$key=$(echo "$extra_context" | jq -r ".$key")")
+  done
+
+  # Call cookiecutter with extra context arguments
+
+  echo "cookiecutter --no-input $cookie_cutter_template $args"
+
+  # Call cookiecutter with extra context arguments
+
+  if [ -n "$template_directory" ]; then
+    cookiecutter --no-input $cookie_cutter_template --directory $template_directory "${args[@]}"
+  else
+    cookiecutter --no-input $cookie_cutter_template "${args[@]}"
+  fi
+}
+
+
+push_to_repository() {
+  if [ -n "$monorepo_url" ] && [ -n "$scaffold_directory" ]; then
+    git config user.name "GitHub Actions Bot"
+    git config user.email "github-actions[bot]@users.noreply.github.com"
+    git add .
+    git commit -m "Scaffolded project in $scaffold_directory"
+    git push -u origin $branch_name
+
+    send_log "Creating pull request to merge $branch_name into main 🚢"
+
+    owner=$(echo "$monorepo_url" | awk -F'/' '{print $4}')
+    repo=$(echo "$monorepo_url" | awk -F'/' '{print $5}')
+
+    echo "Owner: $owner"
+    echo "Repo: $repo"
+
+    PR_PAYLOAD=$(jq -n --arg title "Scaffolded project in $repo" --arg head "$branch_name" --arg base "main" '{
+      "title": $title,
+      "head": $head,
+      "base": $base
+    }')
+
+    echo "PR Payload: $PR_PAYLOAD"
+
+    pr_url=$(curl -X POST \
+      -H "Authorization: token $github_token" \
+      -H "Content-Type: application/json" \
+      -d "$PR_PAYLOAD" \
+      "$git_url/repos/$owner/$repo/pulls" | jq -r '.html_url')
+
+    send_log "Opened a new PR in $pr_url 🚀"
+    add_link "$pr_url"
+
+    else
+      cd "$(ls -td -- */ | head -n 1)"
+      git init
+      git config user.name "GitHub Actions Bot"
+      git config user.email "github-actions[bot]@users.noreply.github.com"
+      git add .
+      git commit -m "Initial commit after scaffolding"
+      git branch -M main
+      git remote add origin https://oauth2:$github_token@github.com/$org_name/$repository_name.git
+      git push -u origin main
+  fi
+}
+
+
+report_to_port() {
+  curl --location "https://api.getport.io/v1/blueprints/$blueprint_identifier/entities?run_id=$port_run_id" \
+    --header "Authorization: Bearer $access_token" \
+    --header "Content-Type: application/json" \
+    --data "{
+      \"identifier\": \"$repository_name\",
+      \"title\": \"$repository_name\",
+      \"properties\": {}
+    }"
 }
 
 main() {

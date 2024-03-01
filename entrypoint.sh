75fbdc87ced48a9b5f03c405316d48bbff48f913
pcb737 <63125424+pcb737@users.noreply.github.com>
2024-03-01 12:49:33 -0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -73,10 +73,6 @@ clone_monorepo() {
   git checkout -b $branch_name
 }
 
-prepare_cookiecutter_extra_context() {
-  echo "$port_user_inputs" | jq -r 'with_entries(select(.key | startswith("cookiecutter_")) | .key |= sub("cookiecutter_"; ""))'
-}
-
 cd_to_scaffold_directory() {
   if [ -n "$monorepo_url" ] && [ -n "$scaffold_directory" ]; then
     cd $scaffold_directory

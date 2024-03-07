e01436d3468521330846cafd045e8bf3ed1af3e7
pcb737 <63125424+pcb737@users.noreply.github.com>
2024-03-07 11:15:08 -0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -35,14 +35,11 @@ echo "create_port_entity: $create_port_entity"
 echo "branch_name: $branch_name"
 echo "git_url: $git_url"
 
-get_access_token() {
   curl -s --location --request POST 'https://api.getport.io/v1/auth/access_token' --header 'Content-Type: application/json' --data-raw "{
     \"clientId\": \"$port_client_id\",
     \"clientSecret\": \"$port_client_secret\"
   }" | jq -r '.accessToken'
-}
 
-send_log() {
   message=$1
   curl --location "https://api.getport.io/v1/actions/runs/$port_run_id/logs" \
     --header "Authorization: Bearer $access_token" \
@@ -50,9 +47,8 @@ send_log() {
     --data "{
       \"message\": \"$message\"
     }"
-}
 
-add_link() {
+
   url=$1
   curl --request PATCH --location "https://api.getport.io/v1/actions/runs/$port_run_id" \
     --header "Authorization: Bearer $access_token" \
@@ -60,9 +56,7 @@ add_link() {
     --data "{
       \"link\": \"$url\"
     }"
-}
 
-create_repository() {  
   resp=$(curl -H "Authorization: token $github_token" -H "Accept: application/json" -H "Content-Type: application/json" $git_url/users/$org_name)
 
   userType=$(jq -r '.type' <<< "$resp")
@@ -82,25 +76,23 @@ create_repository() {
   else
     echo "Invalid user type"
   fi
-}
 
-clone_monorepo() {
+
+
   git clone $monorepo_url monorepo
   cd monorepo
   git checkout -b $branch_name
-}
 
-prepare_cookiecutter_extra_context() {
+
   echo "$port_user_inputs" | jq -r 'with_entries(select(.key | startswith("cookiecutter_")) | .key |= sub("cookiecutter_"; ""))'
-}
 
-cd_to_scaffold_directory() {
+
+
   if [ -n "$monorepo_url" ] && [ -n "$scaffold_directory" ]; then
     cd $scaffold_directory
   fi
-}
 
-apply_cookiecutter_template() {
+
   extra_context=$(prepare_cookiecutter_extra_context)
 
   echo "🍪 Applying cookiecutter template $cookie_cutter_template with extra context $extra_context"
@@ -128,41 +120,4 @@ apply_cookiecutter_template() {
     echo "❌ Error: Failed to apply cookiecutter template."
     # Trate o erro conforme necessário
   fi
-}
 
-main() {
-  access_token=$(get_access_token)
-
-  if [ -z "$monorepo_url" ] || [ -z "$scaffold_directory" ]; then
-    send_log "Creating a new repository: $repository_name 🏃"
-    create_repository
-    send_log "Created a new repository at https://github.com/$org_name/$repository_name 🚀"
-  else
-    send_log "Using monorepo scaffolding 🏃"
-    clone_monorepo
-    cd_to_scaffold_directory
-    send_log "Cloned monorepo and created branch $branch_name 🚀"
-  fi
-
-  send_log "Starting templating with cookiecutter 🍪"
-  apply_cookiecutter_template
-  send_log "Create template sucessefull 👍"
-
-  url="https://github.com/$org_name/$repository_name"
-
-  if [[ "$create_port_entity" == "true" ]]
-  then
-    send_log "Reporting to Port the new entity created 🚢"
-    report_to_port
-  else
-    send_log "Skipping reporting to Port the new entity created 🚢"
-  fi
-
-  if [ -n "$monorepo_url" ] && [ -n "$scaffold_directory" ]; then
-    send_log "Finished! 🏁✅"
-  else
-    send_log "Finished! Visit $url 🏁✅"
-  fi
-}
-
-main
